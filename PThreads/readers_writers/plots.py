b1e22bf8aa6100cafc75d1752f87442fd80c816e
Giorgos Nikolaou <8georgenik@gmail.com>
2023-12-04 22:39:47 +0200

    1.3 done

---
+++ PThreads/readers_writers/plots.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import itertools
+import subprocess
+import time
+
+initial_insertions = 1000
+operations = 500000
+
+names = ['method', 'member', 'insert', 'threads', 'time']
+param_grid = [
+    ['./rp', './wp'],
+    ['0.9', '0.95', '0.999'],
+    ['0.001', '0.005', '0.05'],
+    ['2', '4', '8', '16']
+]
+
+# df = pd.DataFrame(columns=names)
+
+# for i, (method, members, inserts, threads) in enumerate(list(itertools.product(*param_grid))):
+#     if float(members) + float(inserts) > 1:
+#         continue
+
+#     ops = f'{initial_insertions}\n{operations}\n{members}\n{inserts}\n'
+#     start_time = time.time()
+#     process = subprocess.run([method, threads], input=ops, text=True)
+#     end_time = time.time() - start_time
+    
+#     new_row = pd.DataFrame({key: [val] for key, val in zip(names, [method, members, inserts, threads, end_time])})
+#     df = pd.concat([df, new_row], ignore_index=True)
+
+# print(df)
+
+# df.to_csv('./output/results.csv', index=False)
+
+
+
+dfs = pd.read_csv('./output/results.csv')
+for df_mem in [group for _, group in dfs.groupby(['member', 'insert'])]:
+    
+    members = df_mem["member"].values[0]
+    insert = df_mem["insert"].values[0]
+
+    fig, ax = plt.subplots()
+    ax.grid(visible=True)
+    ax.set_title(f'Member() / Insert() Percentages: {members * 100 :.2f} % / {insert * 100 :.2f} %')
+    ax.set_xlabel('threads')
+    ax.set_ylabel('time (sec)')
+
+    for df in [group for _, group in df_mem.groupby(['method'])]:
+        ax.plot(df['threads'], df['time'], '.-', label=f'Method: {df["method"].values[0][2:]}')
+
+    ax.legend(loc="best")
+    plt.savefig(f'./output/plot{members}-{insert}.png', bbox_inches='tight')
+    plt.close()
