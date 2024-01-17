8197985cf126f53ad619778f9ee2914531fe92e7
Giorgos Nikolaou <8georgenik@gmail.com>
2024-01-17 19:47:09 +0200

    oopsies

--- MPI/monte_carlo/monte_carlo.c
+++ MPI/monte_carlo/monte_carlo.c
@@ -3,9 +3,8 @@
 #include <math.h>
 #include <pthread.h>
 #include <mpi.h>
-#include "timer.h"
+#include "timer.h" 
 
-#define ROOT 0
 #define LONG long long
 
 int main(int argc, char* argv[]) {
@@ -19,20 +18,22 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if(rank == ROOT){
+    if(rank == 0) {
         fprintf(stdout, "Give number of arrow to be thrown: ");
         scanf("%lld", &n);
         
         GET_TIME(start);
+
+        n /= comm_sz;
     }
 
-    MPI_Bcast(&n, 1, MPI_LONG, ROOT, MPI_COMM_WORLD);
+    MPI_Bcast(&n, 1, MPI_LONG, 0, MPI_COMM_WORLD);
 
     LONG arrows = 0;
 
     double range = (1.0 - (-1.0)); 
     double div = RAND_MAX / range;
-    unsigned int mystate = time(NULL) ^ time(rank);
+    unsigned int mystate = time(NULL) ^ rank;
 
     for (LONG i = 0; i < n; i++) {
         double x = -1.0 + (rand_r(&mystate) / div);
@@ -42,26 +43,26 @@ int main(int argc, char* argv[]) {
             arrows++;
     }
 
-    MPI_Reduce(&arrows, &total_arrows, 1, MPI_LONG, MPI_SUM, ROOT, MPI_COMM_WORLD);
+    MPI_Reduce(&arrows, &total_arrows, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if(rank == ROOT) {
+    if(rank == 0) {
 
         GET_TIME(finish);
         elapsed = finish - start;
 
-        double pi = 4 * arrows_in_circle / (double) n;
+        double pi = 4 * total_arrows / ((double) n * comm_sz);
         
         fprintf(stdout, "+-----------+------------+---------------+\n");
         fprintf(stdout, "| Processes |     pi     | elapsed (sec) |\n");
         fprintf(stdout, "|-----------+------------+---------------|\n");
-        fprintf(stdout, "|    %2ld     | %.8f | %13.8f |\n", comm_sz, pi, elapsed);
-        fprintf(stdout, "+-----------+------------+---------------+\n");
+        fprintf(stdout, "|    %2d     | %.8f | %13.8f |\n", comm_sz, pi, elapsed);
+        fprintf(stdout, "+-----------+-------------+---------------+\n");
 
         
         if (argc == 2) {
             FILE* file = fopen(argv[1], "a");
 
-            fprintf(file, "%lld,%ld,%f\n", n, comm_sz, elapsed);
+            fprintf(file, "%lld,%d,%f\n", n, comm_sz, elapsed);
 
             fclose(file);
         }
